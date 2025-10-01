d06a560394af339a82371468f078d4a7fb737454
manuel-Igtm <njoguimmanuel8@gmail.com>
2025-10-01 17:37:49 +0300

    permissions

--- project-nexus/api/urls.py
+++ project-nexus/api/urls.py
@@ -1,5 +1,5 @@
 from rest_framework.routers import DefaultRouter
-from .views import (ProductViewSet,  CategoryViewSet,OrderViewSet,CartViewSet,OrderItemViewSet,CartItemViewSet,PaymentViewSet,NotificationViewSet)
+from api.views import (ProductViewSet,  CategoryViewSet,OrderViewSet,CartViewSet,OrderItemViewSet,CartItemViewSet,PaymentViewSet,NotificationViewSet)
 from django.urls import path,include
 from drf_yasg.views import get_schema_view
 from drf_yasg import openapi
